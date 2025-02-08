8f2135bb793165ae31edcfb690a2155051ee8d64
thinkido <4077725@qq.com>
2025-02-08 16:57:41 +0800

    d 1、项目配置文件，初始化

--- examples/sound.js
+++ examples/sound.js
@@ -4,9 +4,10 @@ Sound.playCaptcha();
 
 console.log("Playing done sound...")
 // Custom sounds will also have the 2-second delay
-// Sound.play("public/done.mp3");
-// await sleep(2000);
+Sound.play("public/done.mp3");
+await sleep(2000);
 
+console.log("playSuccess sound...")
 // These will now have a 2-second pause between sounds
 Sound.playSuccess(); // Plays done.mp3
 await sleep(2000);
@@ -14,8 +15,6 @@ Sound.playFail();    // Plays fail.mp3
 await sleep(2000);
 Sound.playWarning(); // Plays ding.mp3
 await sleep(2000);
-Sound.playError();   // Plays captcha.mp3
-
 
 // Custom sounds will also have the 2-second delay
 Sound.playSound("public/ding.mp3");
---
+++ tm.config.js
@@ -0,0 +1,1 @@
+console.log('tm.config.js loaded，可以在这里放不同项目的业务逻辑代码.')
